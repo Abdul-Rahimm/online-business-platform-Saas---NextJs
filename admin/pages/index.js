a82c2f902e9a3a772ba817b90cc6e8a787c3fa97
Abdul-Rahimm <k213951@nu.edu.pk>
2024-08-12 01:03:57 +0500

    login and logout done

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,17 +1,34 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
- return (
-  <div className="bg-blue-900 w-screen h-screen flex items-center">
+  const { data: session } = useSession();
 
-    <div className="text-center w-full">
-      <button className="bg-white p-2 px-4 rounded-md">
-        Login with Google
-      </button>
-    </div>
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-md"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
 
-    <div>
-      New laptop
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <div className="text-white py-2">Logged In {session.user.name}</div>
+        <button
+          className="bg-white p-2 px-4 rounded-md"
+          onClick={() => signOut()}
+        >
+          signOut
+        </button>
+      </div>
     </div>
-    
-  </div>
- )
+  );
 }
