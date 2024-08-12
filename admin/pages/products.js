497a9c31d7f29bef8096eabf8c9c20a707bb1193
Abdul-Rahimm <k213951@nu.edu.pk>
2024-08-12 14:09:53 +0500

    add new product UI made

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -4,10 +4,7 @@ import Link from "next/link";
 export default function Products() {
   return (
     <Layout>
-      <Link
-        href={"/products/new"}
-        className="bg-blue-900 text-white rounded-md px-2 py-1"
-      >
+      <Link href={"/products/new"} className="btn-primary">
         Add New Product
       </Link>
     </Layout>
--- admin/pages/products/new.js
+++ admin/pages/products/new.js
@@ -1,5 +1,20 @@
 import Layout from "@/components/Layout";
 
 export default function NewProduct() {
-  return <Layout>New Product</Layout>;
+  return (
+    <Layout>
+      <h1>New Product</h1>
+
+      <label>Product Name</label>
+      <input type="text" placeholder="Product Name" />
+
+      <label>Description</label>
+      <textarea placeholder="Add Description"></textarea>
+
+      <label>Price (in USD)</label>
+      <input type="number" placeholder="$ Price" />
+
+      <button className="btn-primary">Save</button>
+    </Layout>
+  );
 }
