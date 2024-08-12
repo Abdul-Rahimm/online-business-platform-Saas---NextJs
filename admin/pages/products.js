b1647905ec6725732106364876c983de58ed4414
Abdul-Rahimm <k213951@nu.edu.pk>
2024-08-12 13:19:16 +0500

    moving on to implement the add product functionality

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -1,5 +1,15 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
-  return <Layout>Products</Layout>;
+  return (
+    <Layout>
+      <Link
+        href={"/products/new"}
+        className="bg-blue-900 text-white rounded-md px-2 py-1"
+      >
+        Add New Product
+      </Link>
+    </Layout>
+  );
 }
