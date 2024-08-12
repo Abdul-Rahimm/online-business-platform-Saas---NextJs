1bfdcca7d85c23c8d1d08db2ff67e34800973e16
Abdul-Rahimm <k213951@nu.edu.pk>
2024-08-12 15:02:06 +0500

    able to send the request for creating a new product. its not being saved rn

--- admin/pages/products/new.js
+++ admin/pages/products/new.js
@@ -1,20 +1,50 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
 
 export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+
+  async function createProduct(event) {
+    event.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+  }
+
   return (
     <Layout>
-      <h1>New Product</h1>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
 
-      <label>Product Name</label>
-      <input type="text" placeholder="Product Name" />
+        <label>Product Name</label>
+        <input
+          type="text"
+          placeholder="Product Name"
+          title={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
 
-      <label>Description</label>
-      <textarea placeholder="Add Description"></textarea>
+        <label>Description</label>
+        <textarea
+          placeholder="Add Description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
 
-      <label>Price (in USD)</label>
-      <input type="number" placeholder="$ Price" />
+        <label>Price (in USD)</label>
+        <input
+          type="number"
+          placeholder="$ Price"
+          value={price}
+          onChange={(event) => setPrice(event.target.value)}
+        />
 
-      <button className="btn-primary">Save</button>
+        <button className="btn-primary" type="submit">
+          Save
+        </button>
+      </form>
     </Layout>
   );
 }
