10b11ea963116480abf9a0066d82a1be8e2b14d2
Abdul Rahim <k213951@nu.edu.pk>
2024-08-29 18:54:30 +0500

    making cateogires now

---
+++ admin/pages/categories.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+
+export default function Categories() {
+  return (
+    <Layout>
+      <h1>Categories</h1>
+
+      <label>Category</label>
+      <div className="flex">
+        <input type="text" placeholder="Category Name" />
+      </div>
+    </Layout>
+  );
+}
