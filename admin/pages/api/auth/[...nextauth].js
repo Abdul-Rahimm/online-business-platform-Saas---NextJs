68fc172e2b5de6448975d28b6ea124a4708852c0
Abdul-Rahimm <k213951@nu.edu.pk>
2024-09-30 00:41:16 +0500

    implementing security 5 29 00

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "../../../lib/mongodb";
 
-export default NextAuth({
+const adminEmails = ["abdulraheemar611@gmail.com", "k213951@nu.edu.pk"];
+
+export const authOptions = {
   adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
@@ -11,4 +13,20 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-});
+  callbacks: {
+    async session({ session, token, user }) {
+      console.log("session ", { session, token, user });
+
+      if (adminEmails.includes(session?.user?.email)) return session;
+      else return false;
+    },
+  },
+};
+
+export async function isAdminRequest(req, res) {
+  const session = getServerSession(req, res, authOptions);
+
+  if (!adminEmails.includes(session?.user.email)) throw "not admin";
+}
+
+export default NextAuth(authOptions);
