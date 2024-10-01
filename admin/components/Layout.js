d2bea8ea405709b81e26347928866b264f7851b7
Abdul-Rahimm <k213951@nu.edu.pk>
2024-10-01 12:02:33 +0500

    6 04 00

--- admin/components/Layout.js
+++ admin/components/Layout.js
@@ -1,8 +1,11 @@
 import Nav from "@/components/nav";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({ children }) {
   const { data: session } = useSession();
+  const [showNav, setShowNav] = useState(false);
 
   if (!session) {
     return (
@@ -20,10 +23,34 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div className="bg-customGray min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4 ">
-        {children}
+    <div className="bg-customGray min-h-screen">
+      {/* showNav button */}
+      <div className="block md:hidden flex items-center p-4">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+
+      {/* actual nav and page */}
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="flex-grow p-4 ">{children}</div>
       </div>
     </div>
   );
