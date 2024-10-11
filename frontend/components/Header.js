afd0fc70b9dce099bb4390000dbafd63e5d4fe76
Abdul-Rahimm <k213951@nu.edu.pk>
2024-10-11 23:51:55 +0500

    created the Center component to center things

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,22 +1,31 @@
 import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
+import Center from "./Center";
 
 const StyledHeader = styled.header`
   background-color: #222;
+  padding: 4px;
+`;
+
+const Logo = styled(Link)`
+  color: #fff;
+  text-decoration: none;
 `;
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Link href={"/"}>Fast-Commerce</Link>
-      <nav>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/products"}>Products</Link>
-        <Link href={"/categories"}>Categories</Link>
-        <Link href={"/account"}>Account</Link>
-        <Link href={"/cart"}>Cart (0)</Link>
-      </nav>
+      <Center>
+        <Logo href={"/"}>Fast-Commerce</Logo>
+        <nav>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/products"}>Products</Link>
+          <Link href={"/categories"}>Categories</Link>
+          <Link href={"/account"}>Account</Link>
+          <Link href={"/cart"}>Cart (0)</Link>
+        </nav>
+      </Center>
     </StyledHeader>
   );
 };
