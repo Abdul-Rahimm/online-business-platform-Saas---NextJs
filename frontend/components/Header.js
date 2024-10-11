a0e7abf031e46c13a4201e8cc9ae7b5c921f1546
Abdul-Rahimm <k213951@nu.edu.pk>
2024-10-11 23:34:32 +0500

    boilerplate code

---
+++ frontend/components/Header.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <Link href={"/"}>Fast-Commerce</Link>
+    </header>
+  );
+};
+
+export default Header;
