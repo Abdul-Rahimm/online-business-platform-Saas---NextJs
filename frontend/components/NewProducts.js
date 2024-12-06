1a4c8bbc47f6f3f691d911e24fbd3ada183eb4a8
Abdul-Rahimm <k213951@nu.edu.pk>
2024-12-06 15:08:30 +0500

    basic product display has been made. now needs to be made responsive

--- frontend/components/NewProducts.js
+++ frontend/components/NewProducts.js
@@ -1,21 +1,27 @@
 import styled from "styled-components";
+import ProductBox from "@/components/ProductBox";
 import Center from "./Center";
-import ProductBox from "./ProductBox";
+//added the center component
 
-const ProductsGrid = styled.div`
+const StyledProductsGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  margin-top: 10px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 `;
 
-export default function NewProducts({ products }) {
-  console.log(products);
+export default function ProductsGrid({ products }) {
   return (
     <Center>
-      <ProductsGrid>
-        {products.map((product) => (
-          <ProductBox {...product} />
-        ))}
-      </ProductsGrid>
+      <StyledProductsGrid>
+        {products?.length > 0 &&
+          products.map((product) => (
+            <ProductBox key={product._id} {...product} />
+          ))}
+      </StyledProductsGrid>
     </Center>
   );
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   body {
-    background-color: #f0f0f0;
+    background-color: #eee;
     padding: 0;
     margin: 0;
     font-family: 'Roboto', 'sans-serif';
