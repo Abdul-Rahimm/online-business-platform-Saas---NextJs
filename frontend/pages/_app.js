3955b39a4b856d2c686ce1cef615ca3618eed162
Abdul-Rahimm <k213951@nu.edu.pk>
2024-12-06 15:20:09 +0500

    google font injection

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  body {
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+  body{
     background-color: #eee;
-    padding: 0;
-    margin: 0;
-    font-family: 'Roboto', 'sans-serif';
+    padding:0;
+    margin:0;
+    font-family: 'Poppins', sans-serif;
   }
 `;
 
