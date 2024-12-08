a364cc114e9ee04f4514d03bddb48b34a55d3a55
Abdul-Rahimm <k213951@nu.edu.pk>
2024-12-08 13:34:34 +0500

    new products are being displayed. not responsive yet.
    now lets add our products to cart

--- frontend/components/NewProducts.js
+++ frontend/components/NewProducts.js
@@ -7,15 +7,21 @@ const StyledProductsGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 20px;
-  margin-top: 10px;
   @media screen and (min-width: 768px) {
     grid-template-columns: 1fr 1fr 1fr 1fr;
   }
 `;
 
+const Title = styled.h2`
+  font-size: 2rem;
+  margin: 30px 0 20px;
+  font-weight: normal;
+`;
+
 export default function ProductsGrid({ products }) {
   return (
     <Center>
+      <Title>New Arrivals</Title>
       <StyledProductsGrid>
         {products?.length > 0 &&
           products.map((product) => (
