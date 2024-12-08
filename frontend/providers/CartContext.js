5dba5ec190e81ef4b6554ef29795d739746e5b2e
Abdul-Rahimm <k213951@nu.edu.pk>
2024-12-08 14:35:10 +0500

    VERY IMPORTANT. cart doesnt get flushed when page is reloaded.

--- frontend/providers/CartContext.js
+++ frontend/providers/CartContext.js
@@ -1,9 +1,25 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext({});
 
 const CartContextProvider = ({ children }) => {
+  const ls = typeof window !== "undefined" ? window.localStorage : null;
+
+  //if items are in the local storage fetch them from there
   const [cartProducts, setCartProducts] = useState([]);
+
+  //so that items do not get lost when page gets refreshed
+  useEffect(() => {
+    if (cartProducts?.length > 0) {
+      ls?.setItem("cart", JSON.stringify(cartProducts));
+    }
+  }, [cartProducts]);
+
+  useEffect(() => {
+    if (ls && ls.getItem("cart")) {
+      setCartProducts(JSON.parse(ls.getItem("cart")));
+    }
+  }, []);
 
   function addProduct(productID) {
     setCartProducts((prev) => [...prev, productID]);
