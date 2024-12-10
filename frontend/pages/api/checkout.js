b3a2fad5004be16d1359d9159064faa6d283e78a
Abdul Rahim <k213951@nu.edu.pk>
2024-12-10 15:14:57 +0500

    BUG FIXED. upon continue to payment, we proceed to stripe page

--- frontend/pages/api/checkout.js
+++ frontend/pages/api/checkout.js
@@ -1,5 +1,7 @@
 import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
 import { Product } from "@/models/product";
+const stripe = require("stripe")(process.env.STRIPE_SK);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
@@ -8,10 +10,17 @@ export default async function handler(req, res) {
   await mongooseConnect();
 
   //products is a string of IDS (comma seperated)
-  const { name, email, city, postalCode, streetAddress, country, products } =
-    req.body;
+  const {
+    name,
+    email,
+    city,
+    postalCode,
+    streetAddress,
+    country,
+    cartProducts,
+  } = req.body;
 
-  const productIds = products?.split(",");
+  const productIds = cartProducts;
 
   //i need to find unique ids
   //two products with the same id means i want two quantity of that product
@@ -39,6 +48,31 @@ export default async function handler(req, res) {
       });
     }
   }
+
+  const orderDocument = await Order.create({
+    line_items,
+    name,
+    email,
+    city,
+    postalCode,
+    streetAddress,
+    country,
+    paid: false,
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    customer_email: email,
+    success_url: process.env.PUBLIC_URL + "/cart?success=1",
+    cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
+    metadata: { orderId: orderDocument._id.toString() },
+  });
+
+  console.log("RL", session.url);
+  res.json({
+    url: session.url,
+  });
 
   res.json({ line_items });
 }
