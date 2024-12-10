ad62bb0035f1844580b46deef1ad17abf6b256c4
Abdul Rahim <k213951@nu.edu.pk>
2024-12-10 13:46:46 +0500

    made a set to store unique product IDS.

--- frontend/pages/api/checkout.js
+++ frontend/pages/api/checkout.js
@@ -1,9 +1,11 @@
-import React from "react";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/product";
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method !== "POST") {
     res.json("Should be a post request");
   }
+  await mongooseConnect();
 
   //products is a string of IDS (comma seperated)
   const { name, email, city, postalCode, streetAddress, country, products } =
@@ -13,6 +15,10 @@ const handler = (req, res) => {
 
   //i need to find unique ids
   //two products with the same id means i want two quantity of that product
+
+  const uniqueIds = [...new Set(productIds)];
+
+  const productInfos = await Product.find({ _id: uniqueIds });
 };
 
 export default handler;
