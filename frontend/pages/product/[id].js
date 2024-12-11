d1ab7cc65413be1b6ad2f0645b26aa03e3346c92
Abdul Rahim <k213951@nu.edu.pk>
2024-12-11 14:10:46 +0500

    need to make it serverSide component

--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -1,15 +1,71 @@
-"use client";
-import { usePathname, useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
-import React from "react";
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/product";
+import styled from "styled-components";
+import WhiteBox from "@/components/WhiteBox";
+import ProductImages from "@/components/ProductImages";
+import Button from "@/components/Button";
+import CartIcon from "@/components/icons/CartIcon";
+import { useCart } from "@/providers/CartContext";
 
-const SingleProductPage = () => {
-  const id = usePathname().split("/")[2];
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 0.8fr 1.2fr;
+  }
+  gap: 40px;
+  margin: 40px 0;
+`;
+const PriceRow = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+const Price = styled.span`
+  font-size: 1.4rem;
+`;
+
+export default function ProductPage({ product }) {
+  const { addProduct } = useCart();
   return (
-    <div>
-      <h1>{id}</h1>
-    </div>
+    <>
+      <Header />
+      <Center>
+        <ColWrapper>
+          <WhiteBox>
+            <ProductImages images={product.images} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title}</Title>
+            <p>{product.description}</p>
+            <PriceRow>
+              <div>
+                <Price>${product.price}</Price>
+              </div>
+              <div>
+                <Button primary onClick={() => addProduct(product._id)}>
+                  <CartIcon />
+                  Add to cart
+                </Button>
+              </div>
+            </PriceRow>
+          </div>
+        </ColWrapper>
+      </Center>
+    </>
   );
-};
+}
 
-export default SingleProductPage;
+export async function getServerSideProps(context) {
+  await mongooseConnect();
+  const { id } = context.query;
+  const product = await Product.findById(id);
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
+}
