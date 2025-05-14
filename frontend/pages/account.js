7dc35639e1ca3d91ad8e56d3dede79ae40cb0aba
Abdul-Rahimm <k213951@nu.edu.pk>
2025-05-14 17:25:00 +0500

    UI basic made for Account page. Structure wise

--- frontend/pages/account.js
+++ frontend/pages/account.js
@@ -1,12 +1,34 @@
 import Button from "@/components/Button";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import Input from "@/components/Input";
 import Title from "@/components/Title";
+import WhiteBox from "@/components/WhiteBox";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import styled from "styled-components";
 
+const ColsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1.2fr 0.8fr;
+  gap: 40px;
+  margin: 40px 0;
+`;
+const CityHolder = styled.div`
+  display: flex;
+  gap: 5px;
+`;
 export default function AccountPage() {
   const { data: session } = useSession();
-  console.log("session data", session);
+
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [country, setCountry] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
 
   async function logout() {
     await signOut({
@@ -15,27 +37,84 @@ export default function AccountPage() {
   }
 
   async function login() {
-    await signIn("google", {
-      callbackUrl: process.env.NEXT_PUBLIC_URL,
-    });
+    await signIn("google");
   }
 
   return (
     <>
       <Header />
       <Center>
-        <Title>Account</Title>
-        {session && (
-          <Button primary={1} onClick={() => logout()}>
-            Logout
-          </Button>
-        )}
+        <ColsWrapper>
+          <div>
+            <WhiteBox>
+              <h2>Wish List</h2>
+            </WhiteBox>
+          </div>
+          <div>
+            <WhiteBox>
+              <h2>Account Details</h2>
+              <Input
+                type="text"
+                placeholder="Name"
+                value={name}
+                name="name"
+                onChange={(ev) => setName(ev.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
+              <CityHolder>
+                <Input
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  name="city"
+                  onChange={(ev) => setCity(ev.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Postal Code"
+                  value={postalCode}
+                  name="postalCode"
+                  onChange={(ev) => setPostalCode(ev.target.value)}
+                />
+              </CityHolder>
+              <Input
+                type="text"
+                placeholder="Street Address"
+                value={streetAddress}
+                name="streetAddress"
+                onChange={(ev) => setStreetAddress(ev.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Country"
+                value={country}
+                name="country"
+                onChange={(ev) => setCountry(ev.target.value)}
+              />
+              <Button black block>
+                Save
+              </Button>
+              <hr />
+              {session && (
+                <Button primary={1} onClick={logout}>
+                  Logout
+                </Button>
+              )}
 
-        {!session && (
-          <Button primary={1} onClick={login}>
-            Log in
-          </Button>
-        )}
+              {!session && (
+                <Button primary={1} onClick={login}>
+                  Log in
+                </Button>
+              )}
+            </WhiteBox>
+          </div>
+        </ColsWrapper>
       </Center>
     </>
   );
